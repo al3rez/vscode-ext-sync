f65435a6e463da910c8500454512117446198b4d
Alireza Bashiri <azbshiri@gmail.com>
2019-02-14 18:15:46 +0700

    add vs code extension manager

---
+++ bin/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+command -v code &>/dev/null || { echo -e "🥊 code is is not installed\n"; exit 1; }
+
+echo -e "✨ installing extensions"
+
+cat extensions.txt | while read ext; do
+  code --install-extension $ext &>/dev/null && { echo -e "✅ $ext is installed "; }
+done
