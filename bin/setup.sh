eed288a84c9e03e433033c68528abdcbc088a134
Alireza Bashiri <azbshiri@gmail.com>
2019-02-15 00:32:06 +0700

    use git to read extensions.txt

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
+[[ $OSTYPE =~ darwin ]] || { echo -e "😞 sorry this script works only on mac"; exit 1; }
 
-command -v code &>/dev/null || { echo -e "🥊 code is is not installed"; exit 1; }
+command -v code &>/dev/null || { echo -e "🦄 installing vs code"; } && brew cask install visual-studio-code &>/dev/null
 
 echo -e "✨ installing extensions"
-
-cat extensions.txt | while read ext; do
+cat "$(git rev-parse --show-toplevel)/extensions.txt" | while read ext; do
   code --install-extension $ext &>/dev/null && { echo -e "✅ $ext is installed "; }
 done
