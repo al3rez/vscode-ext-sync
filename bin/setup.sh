2a09bb08b712be5d2bdf617fd43c9c42997a9df0
Alireza Bashiri <azbshiri@gmail.com>
2019-02-15 00:39:15 +0700

    fmt ./bin/script.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-[[ $OSTYPE =~ darwin ]] || { echo -e "😞 sorry this script works only on mac"; exit 1; }
+[[ $OSTYPE =~ darwin ]] || \
+	{ echo -e "😞 sorry this script works only on mac"; exit 1; }
 
-command -v code &>/dev/null || { echo -e "🦄 installing vs code"; } && brew cask install visual-studio-code &>/dev/null
+command -v code &>/dev/null || { echo -e "🦄 installing vs code"; } \
+	&& brew cask install visual-studio-code &>/dev/null
 
-echo -e "✨ installing extensions"
 cat "$(git rev-parse --show-toplevel)/extensions.txt" | while read ext; do
+  echo -e "✨ installing extensions"
   code --install-extension $ext &>/dev/null && { echo -e "✅ $ext is installed "; }
 done
+
+exit 0;
